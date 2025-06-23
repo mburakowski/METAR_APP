3da8871fe536505a6c26982f39e2ef92fd904b1c
Porfiruisz <263593@student.pwr.edu.pl>
2025-06-23 20:05:01 +0200

    Update api_metar.kt

--- METAR_decoder/app/src/main/java/com/example/metar_decoder/api_metar.kt
+++ METAR_decoder/app/src/main/java/com/example/metar_decoder/api_metar.kt
@@ -6,8 +6,16 @@ import okhttp3.Callback
 import java.io.IOException
 
 val client = OkHttpClient()
+
+/**
+ * Klucz API do autoryzacji zapytań w usłudze AVWX.
+ */
 val apiKey = "KBMXYEsCFCdkxdAbAagPDwVgN1GH-jtDwn01Wjif_5Y"
 
+/**
+ * Główna funkcja programu. Prosi użytkownika o kod ICAO i pobiera dane METAR oraz TAF
+ * z serwisu AVWX. Następnie wypisuje surową odpowiedź JSON na konsolę.
+ */
 fun main() {
     println("=== METAR Downloader (AVWX API) ===")
     print("Podaj kod ICAO lotniska (np. EPWA): ")
@@ -20,6 +28,12 @@ fun main() {
     Thread.sleep(3000)
 }
 
+/**
+ * Wysyła asynchroniczne zapytanie HTTP GET do API AVWX, aby pobrać dane METAR
+ * dla danego kodu ICAO lotniska. Odpowiedź (surowy JSON) wypisywana jest na konsolę.
+ *
+ * @param stationICAO Czteroliterowy kod ICAO lotniska (np. EPWA).
+ */
 fun getMetarData(stationICAO: String) {
     val url = "https://avwx.rest/api/metar/$stationICAO"
 
@@ -43,6 +57,12 @@ fun getMetarData(stationICAO: String) {
     })
 }
 
+/**
+ * Wysyła asynchroniczne zapytanie HTTP GET do API AVWX, aby pobrać dane TAF
+ * dla danego kodu ICAO lotniska. Odpowiedź (surowy JSON) wypisywana jest na konsolę.
+ *
+ * @param stationICAO Czteroliterowy kod ICAO lotniska (np. EPWA).
+ */
 fun getTafData(stationICAO: String) {
     val url = "https://avwx.rest/api/taf/$stationICAO"
 
